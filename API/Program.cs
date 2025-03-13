e3d3145376d7b59fb4da5564f7b4a3469378db9c
Teo <thod_ntonas@hotmail.com>
2025-03-14 01:14:17 +0200

    Create react project

--- API/Program.cs
+++ API/Program.cs
@@ -11,9 +11,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseCors(options => 
+    options.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000")
+);
 
 app.MapControllers();
 
