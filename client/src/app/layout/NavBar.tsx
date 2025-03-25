a9e052aff88e4569d50e81e07dcaa0c533fa4f6e
Teo <thod_ntonas@hotmail.com>
2025-03-25 15:13:41 +0200

    Add mobx store, improve data fetching with tanstack query

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -6,18 +6,24 @@ import {
   Typography,
   Container,
   MenuItem,
+  LinearProgress,
 } from "@mui/material";
 import { NavLink } from "react-router";
 import MenuItemLink from "../shared/components/MenuItemLink";
+import useStore from "../../lib/hooks/useStore";
+import { Observer } from "mobx-react-lite";
 
 export default function NavBar() {
+  const { uiStore } = useStore();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
         position="static"
         sx={{
           backgroundImage:
-            "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)"
+            "linear-gradient(135deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
+          position: 'relative'
         }}
       >
         <Container maxWidth='xl'>
@@ -35,6 +41,9 @@ export default function NavBar() {
               <MenuItemLink to='/createActivity'>
                 Create Activity
               </MenuItemLink>
+              <MenuItemLink to='/counter'>
+                Counter
+              </MenuItemLink>
             </Box>
             <MenuItem>
               User menu
@@ -42,6 +51,23 @@ export default function NavBar() {
           </Toolbar>
         </Container>
 
+        <Observer>
+          {() =>
+            uiStore.isLoading ? (
+              <LinearProgress
+                color="secondary"
+                sx={{
+                  position: 'absolute',
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  height: 4
+                }}
+              />
+            )
+              : null
+          }
+        </Observer>
       </AppBar>
     </Box>
   );
--- client/src/lib/api/agents.ts
+++ client/src/lib/api/agents.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => new Promise(resolve => setTimeout(resolve, delay))
 
 const agent = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
+
+agent.interceptors.request.use(config => {
+    store.uiStore.isBusy();
+    return config;
+})
 
 agent.interceptors.response.use(async response => {
     try {
@@ -13,6 +19,8 @@ agent.interceptors.response.use(async response => {
     } catch (error) {
         console.log(error)
         return Promise.reject(error)
+    } finally {
+        store.uiStore.isIdle();
     }
 });
 
