c8df06ffbfd5368f31ae853dffda3d21e6a8ffb3
Teo <thod_ntonas@hotmail.com>
2025-03-28 15:01:49 +0200

    Setup  react-hook-form and zod

--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,33 +1,37 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
-import { FormEvent } from "react";
 import { useActivities } from "../../../lib/hooks/useActivities";
-import { useNavigate, useParams } from "react-router";
+import { useParams } from "react-router";
+import { useForm } from 'react-hook-form';
+import { useEffect } from "react";
+import { activitySchema, ActivitySchema } from "../../../lib/schemas/activitySchema";
+import { zodResolver } from '@hookform/resolvers/zod'
 
 export default function ActivityForm() {
+    const { register, reset, handleSubmit, formState: { errors } } = useForm<ActivitySchema>({
+        mode: 'onTouched',
+        resolver: zodResolver(activitySchema)
+    });
     const { id } = useParams();
     const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id)
-    const navigate = useNavigate();
 
-    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault()
+    useEffect(() => {
+        if (activity) reset(activity)
+    }, [activity, reset]);
 
-        const formData = new FormData(event.currentTarget)
-        const data: { [key: string]: FormDataEntryValue } = {}
-        formData.forEach((value, key) => {
-            data[key] = value;
-        })
+    const onSubmit = async (data: ActivitySchema) => {
 
-        if (activity) {
-            data.id = activity.id
-            await updateActivity.mutateAsync(data as unknown as Activity)
-            navigate(`/activities/${activity.id}`)
-        } else {
-            await createActivity.mutateAsync(data as unknown as Activity, {
-                onSuccess: (id: string) => {
-                    navigate(`/activities/${id}`)
-                }
-            })
-        }
+        console.log(data)
+        // if (activity) {
+        //     data.id = activity.id
+        //     await updateActivity.mutateAsync(data as unknown as Activity)
+        //     navigate(`/activities/${activity.id}`)
+        // } else {
+        //     await createActivity.mutateAsync(data as unknown as Activity, {
+        //         onSuccess: (id: string) => {
+        //             navigate(`/activities/${id}`)
+        //         }
+        //     })
+        // }
     }
 
     if (isLoadingActivity) return <Typography>Loading activity...</Typography>
@@ -37,16 +41,22 @@ export default function ActivityForm() {
             <Typography variant="h5" gutterBottom color="primary">
                 {activity ? 'Edit activity' : 'Create activity'}
             </Typography>
-            <Box component='form' onSubmit={handleSubmit} display='flex' flexDirection='column' gap={3}>
-                <TextField name="title" label='Title' defaultValue={activity?.title} />
-                <TextField name="description" label='Description' multiline rows={3} defaultValue={activity?.description} />
-                <TextField name="category" label='Category' defaultValue={activity?.category} />
-                <TextField name="date" label='Date' type="date"
+            <Box component='form' onSubmit={handleSubmit(onSubmit)} display='flex' flexDirection='column' gap={3}>
+                <TextField
+                    {...register("title")}
+                    label='Title'
+                    defaultValue={activity?.title}
+                    error={!!errors.title}
+                    helperText={errors.title?.message} />
+                <TextField {...register("description")} label='Description' multiline rows={3} defaultValue={activity?.description} error={!!errors.description}
+                    helperText={errors.description?.message} />
+                <TextField {...register("category")} label='Category' defaultValue={activity?.category} />
+                <TextField {...register("date")} label='Date' type="date"
                     defaultValue={activity?.date
                         ? new Date(activity.date + 'Z').toISOString().split('T')[0]
                         : new Date().toISOString().split('T')[0]} />
-                <TextField name="city" label='City' defaultValue={activity?.city} />
-                <TextField name="venue" label='Venue' defaultValue={activity?.venue} />
+                <TextField {...register("city")} label='City' defaultValue={activity?.city} />
+                <TextField {...register("venue")} label='Venue' defaultValue={activity?.venue} />
                 <Box display='flex' justifyContent='end' gap={3}>
                     <Button color="inherit">Cancel</Button>
                     <Button
